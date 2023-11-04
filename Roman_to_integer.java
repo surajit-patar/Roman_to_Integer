f1933984861ef1c47534d997711d32dd28dd9796
SURAJIT PATAR <134231529+surajit-patar@users.noreply.github.com>
2023-11-04 15:29:58 +0530

    Update Roman_to_integer.java

--- Roman_to_integer.java
+++ Roman_to_integer.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 import java.util.HashMap;
 
-public class Roman_to_integer{
-    public static void fnConvert(String s){
+class Roman_to_integer{
+    public static void romanToInteger(String s){  //CIV
         HashMap<Character,Integer> map = new HashMap<>();
         map.put('I',1);
         map.put('V',5);
@@ -12,25 +12,56 @@ public class Roman_to_integer{
         map.put('D',500);
         map.put('M',1000);
 
-        int ans = map.get(s.charAt(s.length()-1));
+        int ans = map.get(s.charAt(s.length()-1));  //ans=5 V
         for(int i=s.length()-2;i>=0;i--){
-            if(map.get(s.charAt(i))<map.get(s.charAt(i+1))){
-                ans-=map.get(s.charAt(i));
+            if(map.get(s.charAt(i))<map.get(s.charAt(i+1))){ // 1<5,,, 100<1
+                ans-=map.get(s.charAt(i)); // ANS=5-1; 4
             }
-            else ans+=map.get(s.charAt(i));
+            else ans+=map.get(s.charAt(i)); //100+4=104
         }
 
-        System.out.println("=====\nRoman: "+s);
-        System.out.println("Integer: "+ans+"\n======");
+        System.out.println("=========================================\nRoman: "+s);
+        System.out.println("Integer: "+ans+"\n==================================");
 
+    }
+
+    public static void integerToRoman(int num){
+        int code[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        String s[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        int n=num;
+        String ans="";
+        int i=100;
+        for(i=code.length-1;i>=0;i--){
+            if(num==0) break;
+            while(num>=code[i]){
+                ans+=s[i];
+                num-=code[i];
+            }
+        }
+        System.out.println(i+"\n");
+        System.out.println("==============================\nInteger -> "+n);
+        System.out.println("Roman -> "+ans+"\n===============================");
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(true){
-            System.out.println("Enter the Roman Number here ------- ");
-            String s = sc.nextLine();
-            fnConvert(s);
-        }
+        System.out.println("-------------------WELCOME TO THE INTERFACE-------------------");
+        System.out.println("Press 1 For Integer to Roman!\nPress 2 For Roman to Integer!");
+        int choice = sc.nextInt();
+        switch(choice){
+            case 1:
+            while (true) {
+                System.out.println("Enter the Integer Value ->");
+                int num=sc.nextInt();
+                integerToRoman(num);
+            }
 
+            case 2:
+            while(true){
+                System.out.println("Enter the Roman Value ->");
+                String s = sc.next();
+                romanToInteger(s);
+            }
+
+        }
     }
 }
